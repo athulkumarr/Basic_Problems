df2bbe3ebcdae68a47ac13d36d9e18c208693310
AthulKumar R <36944209+athulkumarr@users.noreply.github.com>
2021-08-05 23:31:46 +0530

    Create MinChairs.java

---
+++ Min Number of Chairs/MinChairs.java
@@ -0,0 +1,29 @@
+class MinChairs
+{
+	private static int minChairs(int[] S, int[] E) 
+	{
+		Arrays.sort(S);
+		Arrays.sort(E);
+
+		int i=0, j=0;
+		int maxChairs=1;
+		int count =0;
+
+		while(i<S.length) 
+		{
+			if(S[i] <E[j]) 
+			{
+				count++;
+				if(count>chairs)				
+					maxChairs = count;
+				i++;
+			}
+			else
+			{
+				count--;
+				j++;
+			}
+		}
+		return maxChairs;
+	}
+}
