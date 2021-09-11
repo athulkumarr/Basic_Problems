bdd6b2506fae8470f64ea6103ccbd05179fe6d50
AthulKumar R <36944209+athulkumarr@users.noreply.github.com>
2021-09-11 22:42:00 +0530

    Update LongestSubstringWoutRepitition.py

--- Longest Substring Without Repetition/LongestSubstringWoutRepitition.py
+++ Longest Substring Without Repetition/LongestSubstringWoutRepitition.py
@@ -1,17 +1,25 @@
 def lengthOfLongestSubstring(self, s: str) -> int:
 		if len(s)==0:
 				return 0
-		start = 0
-		end = 0
-		max_len = 0
-		while start<len(s):
-				if end >= len(s):
-						max_len = max(max_len, end-start)
-						break
-				elif s[end] in s[start:end]:
-						max_len = max(max_len, end-start)
-						start += 1
-						end = start
+		left = 0
+		viewedC = {} # store unique character's index
+		maxLength = 0
+
+		for right, c in enumerate(s):
+				if c not in viewedC:
+						viewedC[c] = right
 				else:
-						end += 1
-		return max_len
+
+						if viewedC[c] < left: 		# update unique character's index
+								viewedC[c] = right
+						else:
+								length = right - left
+								if length > maxLength: 
+										maxLength = length
+								left = viewedC[c] + 1
+
+								viewedC[c] = right	# update unique character's index
+
+		length = len(s[left:])
+
+		return maxLength if maxLength > length else length
